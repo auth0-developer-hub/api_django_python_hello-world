e8c3a44a039fcda4abb6aa07dd0971791625ba8d
Martin Mena <mmena@stackbuilders.com>
2022-01-28 12:40:07 -0500

    Update responses and add docker (#7)

--- messages_api/models.py
+++ messages_api/models.py
@@ -1,4 +1,11 @@
 
 class Message:
     def __init__(self, text=""):
+        self.metadata = Metadata()
         self.text = text
+
+
+class Metadata:
+    def __init__(self):
+        self.api = "api_django_python_hello-world"
+        self.branch = "basic-authorization"
